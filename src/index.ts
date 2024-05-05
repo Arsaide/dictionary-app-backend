90cdf724deed6025da1b32a9ec6362a89115354f
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-05-06 00:35:34 +0300

    - worked with ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 import express from 'express'
-const mongoose = require("mongoose")
-
 const app = express();
-const cors = require("cors")
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+// const cors = require("cors")
+// app.use(cors({
+//     origin: true,
+//     credentials: true
+// }));
 const PORT = process.env.PORT || 6000
 
 app.get('/', (req, res) => {
-    const a = 4;
-    if(a > 5) {
-        res.send('OK');
-    } else {
-        res.send('Hello World')
-    }
+    res.send('Hello World')
+})
+
+app.get('/api', (req, res) => {
+    res.send('Start api')
 })
 
 app.listen(PORT, () => {
